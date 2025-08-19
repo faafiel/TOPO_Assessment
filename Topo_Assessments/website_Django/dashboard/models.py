799f8e5784142782314bfece977659a6d5a60616
faafiel <amirzahed@icloud.com>
2025-08-19 16:46:00 +0800

    now at object creation
    gg

--- Topo_Assessments/website_Django/dashboard/models.py
+++ Topo_Assessments/website_Django/dashboard/models.py
@@ -1,14 +1,56 @@
+import json
+
 from django.db import models
 
 # Create your models here.
-from django.db import models
-from django.contrib.postgres.fields import ArrayField
-
+import os
+from django.conf import settings
 
 class Industry(models.Model):
     name = models.CharField(max_length=200, primary_key=True)
     industry_Performance_List = models.JSONField(null=True, blank=True, default=list)
     company_List = models.JSONField(null=True, blank=True, default=list)
+    # variables
+   
+
+
+    @classmethod
+    def create_Industry (self, file):
+        if self.pk is None:  # create if no exisiting pk
+            name = "1"
+            industry_performance_list = []
+            company_list = []
+
+            # nested for loop to extrat out data from json file and create sub-classes
+            for industry_attribute in file:
+                if industry_attribute == "name":
+                    name = industry_attribute
+
+                elif industry_attribute == 'industry_Performance_List':
+                    industry_performance_list = industry_attribute
+                    for quarter in industry_performance_list:
+                        quarter_attribute_list = []
+                        for quarter_attribute in quarter:
+                            quarter_attribute_list.append(quarter_attribute)
+                        tmp_industry_quarter = Industry_Quarters.create_industry_quarter(name,
+                                                                                         quarter_attribute_list[0],
+                                                                                         quarter_attribute_list[1],
+                                                                                         quarter_attribute_list[2],
+                                                                                         quarter_attribute_list[3],
+                                                                                         quarter_attribute_list[4])
+                        tmp_industry_quarter.save()
+
+                elif industry_attribute == 'company_List':
+                    company_list = industry_attribute 
+                    
+
+
+            tmp_company_obj = Industry(name= name,
+                                      industry_performance_list = [],
+                                      company_list = [])
+            tmp_company_obj.save()
+        # super().save(*args, **kwargs)  # Call the "real" save() method.
+
 
     def __str__(self):
         return self.name
@@ -23,6 +65,13 @@ class Industry_Quarters(models.Model):
     avg_Duration_Mins = models.IntegerField()
     industry = models.ForeignKey(Industry, on_delete=models.CASCADE)
 
+    @classmethod
+    def create_industry_quarter(self, *args, **kwargs):
+        if self.pk is None:  # create
+            tmp_industry_quarter_obj = Industry_Quarters( *args, **kwargs)
+            tmp_industry_quarter_obj.save()
+        super().save(*args, **kwargs)  # Call the "real" save() method.
+
 class Company(models.Model):
     id = models.IntegerField( primary_key=True)
     name = models.CharField(max_length=256)
@@ -33,6 +82,13 @@ class Company(models.Model):
     # q_Performance_List = ArrayField(models.foreignKey(q_Performance, on_delete=models.CASCADE))
     # client_List = models.JSONField(default=list)
     # a_Performance_List = models.JSONField(default=list)
+
+    # @classmethod
+    # def create_company (self, *args, **kwargs):
+    #     if self.pk is None:  # create
+    #         tmp_company_obj = Company(id=)
+    #         tmp_company_obj.save()
+    #     super().save(*args, **kwargs)  # Call the "real" save() method.
 
 class Client(models.Model):
     date = models.CharField(max_length=40)
@@ -48,7 +104,7 @@ class Employee(models.Model):
     name = models.CharField(max_length=50)
     role = models.CharField(max_length=50)
     cashmoney = models.IntegerField()
-    hired_Date = models.CharField(max_length=20)
+    hired_Date = models.CharField(max_length=20, null = True)
     comapany_id = models.ForeignKey(Company, on_delete=models.CASCADE)
 
 class A_Performance(models.Model):
@@ -71,3 +127,12 @@ class Q_Performance(models.Model):
 class Item(models.Model):
     name = models.CharField()
     created = models.DateTimeField(auto_now_add=True)
+
+#=====================================================================================
+# Import and initialise entire db
+file_path = os.path.join(settings.BASE_DIR, 'dashboard', 'unified_Data.json')
+with open(file_path, 'r') as file:
+    data = json.load(file)
+
+
+tmp_industry = Industry.create_Industry(data)
--- Topo_Assessments/website_Django/dashboard/views.py
+++ Topo_Assessments/website_Django/dashboard/views.py
@@ -1,45 +1,151 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from django.template import RequestContext
 
-# Create your views here.
-# from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
 from dashboard.models import *
 from .serializers import *
+from .utils import *
 
 # def index(request):
 #     return render(request, 'index.html')
 
-def index(request):
-  template = loader.get_template('index.html')
-  return HttpResponse(template.render())
 
-# @api_view(['GET'])
-# def getData(request):
-#     items = Item.objects.all()
-#     serializer = Item_Serializer(items, many=True)
-#     return Response(serializer.data)
 
-# @api_view(['POST'])
-# def addItem(request):
-#     serializer = Item_Serializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#     return Response(serializer.data)
+def index_page(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
 
-# def import_data(request):
-#     if request.method == 'POST' and request.FILES['json_file']:
-#         json_file = request.FILES['json_file']
-#         data = json.load(json_file)
-#         for item in data:
-#             book = Book(
-#                 title=item['title'],
-#                 author=item['author'],
-#                 publication_year=item['publication_year']
-#             )
-#             book.save()
-#         return render(request, 'success.html')
-#     return render(request, 'form.html') 
+def charts_page(request):
+    template = loader.get_template('charts.html')
+    return HttpResponse(template.render())
+
+#==================================================================================================
+
+# Here to detect any json file uploaded
+def import_data(request):
+    if request.method == 'POST' and request.FILES['unified_Data.json']:
+        json_file = request.FILES['unified_Data.json']
+        data = json.load(json_file)
+        # for item in data:
+        #     book = Book(
+        #         title=item['title'],
+        #         author=item['author'],
+        #         publication_year=item['publication_year']
+        #     )
+        #     book.save()
+        return render(RequestContext(request), 'success.html')
+    return render(RequestContext(request), 'index.html')
+
+#==================================================================================================
+# Chart related
+
+# def get_filter_options(request):
+#     grouped_purchases = Purchase.objects.annotate(year=ExtractYear("time")).values("year").order_by("-year").distinct()
+#     options = [purchase["year"] for purchase in grouped_purchases]
+
+#     return JsonResponse({
+#         "options": options,
+#     })
+
+
+
+# def get_sales_chart(request, year):
+#     purchases = Purchase.objects.filter(time__year=year)
+#     grouped_purchases = purchases.annotate(price=F("item__price")).annotate(month=ExtractMonth("time"))\
+#         .values("month").annotate(average=Sum("item__price")).values("month", "average").order_by("month")
+
+#     sales_dict = get_year_dict()
+
+#     for group in grouped_purchases:
+#         sales_dict[months[group["month"]-1]] = round(group["average"], 2)
+
+#     return JsonResponse({
+#         "title": f"Sales in {year}",
+#         "data": {
+#             "labels": list(sales_dict.keys()),
+#             "datasets": [{
+#                 "label": "Amount ($)",
+#                 "backgroundColor": colorPrimary,
+#                 "borderColor": colorPrimary,
+#                 "data": list(sales_dict.values()),
+#             }]
+#         },
+#     })
+
+
+
+# def spend_per_customer_chart(request, year):
+#     purchases = Purchase.objects.filter(time__year=year)
+#     grouped_purchases = purchases.annotate(price=F("item__price")).annotate(month=ExtractMonth("time"))\
+#         .values("month").annotate(average=Avg("item__price")).values("month", "average").order_by("month")
+
+#     spend_per_customer_dict = get_year_dict()
+
+#     for group in grouped_purchases:
+#         spend_per_customer_dict[months[group["month"]-1]] = round(group["average"], 2)
+
+#     return JsonResponse({
+#         "title": f"Spend per customer in {year}",
+#         "data": {
+#             "labels": list(spend_per_customer_dict.keys()),
+#             "datasets": [{
+#                 "label": "Amount ($)",
+#                 "backgroundColor": colorPrimary,
+#                 "borderColor": colorPrimary,
+#                 "data": list(spend_per_customer_dict.values()),
+#             }]
+#         },
+#     })
+
+
+
+# def payment_success_chart(request, year):
+#     purchases = Purchase.objects.filter(time__year=year)
+
+#     return JsonResponse({
+#         "title": f"Payment success rate in {year}",
+#         "data": {
+#             "labels": ["Successful", "Unsuccessful"],
+#             "datasets": [{
+#                 "label": "Amount ($)",
+#                 "backgroundColor": [colorSuccess, colorDanger],
+#                 "borderColor": [colorSuccess, colorDanger],
+#                 "data": [
+#                     purchases.filter(successful=True).count(),
+#                     purchases.filter(successful=False).count(),
+#                 ],
+#             }]
+#         },
+#     })
+
+
+
+# def payment_method_chart(request, year):
+#     purchases = Purchase.objects.filter(time__year=year)
+#     grouped_purchases = purchases.values("payment_method").annotate(count=Count("id"))\
+#         .values("payment_method", "count").order_by("payment_method")
+
+#     payment_method_dict = dict()
+
+#     for payment_method in Purchase.PAYMENT_METHODS:
+#         payment_method_dict[payment_method[1]] = 0
+
+#     for group in grouped_purchases:
+#         payment_method_dict[dict(Purchase.PAYMENT_METHODS)[group["payment_method"]]] = group["count"]
+
+#     return JsonResponse({
+#         "title": f"Payment method rate in {year}",
+#         "data": {
+#             "labels": list(payment_method_dict.keys()),
+#             "datasets": [{
+#                 "label": "Amount ($)",
+#                 "backgroundColor": generate_color_palette(len(payment_method_dict)),
+#                 "borderColor": generate_color_palette(len(payment_method_dict)),
+#                 "data": list(payment_method_dict.values()),
+#             }]
+#         },
+#     })
