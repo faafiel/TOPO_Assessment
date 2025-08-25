37c63ba9d82a5539ed2275717487b5c5c306bed5
faafiel <amirzahed@icloud.com>
2025-08-25 11:43:54 +0800

    more change

--- Topo_Assessments/website_Django/dashboard/views.py
+++ Topo_Assessments/website_Django/dashboard/views.py
@@ -9,7 +9,6 @@ from rest_framework.views import APIView
 
 from django.http import JsonResponse
 from dashboard.models import *
-from .serializers import *
 from pathlib import Path
 
 def index_page(request):
